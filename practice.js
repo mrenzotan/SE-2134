554fbef2d37e970f2c4261bb36c38d3d3963b589
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2023-10-12 12:57:50 +0800

    first commit

---
+++ practice.js
@@ -0,0 +1,52 @@
+"use strict";
+let readline = require('readline-sync'); //keep readline.sync for now but not needed in web apps
+const FILENAME = 'debts.txt';
+const fs = require('fs');
+function isNumber(something) {
+    return !Number.isNaN(something);
+}
+function displayDebtors() {
+    console.log('-'.repeat(100));
+    console.log('Dya rn ang mga palautang:');
+    const debtors = fs.readFile(FILENAME + 'g', callMeWhenDoneReadingFile).toString();
+    console.log(debtors);
+    console.log('-'.repeat(100));
+}
+//error first callback
+function callMeWhenDoneWritingFile(error) {
+    // truthy check
+    if (error) {
+        console.log('Something wrong writing the file: ', error.message);
+        process.exit(1);
+    }
+}
+function callMeWhenDoneReadingFile(error, contents) {
+    if (error) {
+        console.log('Something wrong reading the file: ', error.message);
+    }
+    console.log(contents.toString());
+    console.log('-'.repeat(100));
+}
+while (true) {
+    const input = readline.question('Who owes you and how much? ');
+    // input is name[space]amount
+    console.log('The input is', input);
+    if (input === 'walang utang') {
+        break;
+    }
+    if (!input.includes(' ')) {
+        console.log('Invalid input format');
+        continue;
+    }
+    //array destructuring
+    const [_name, amountAsString] = input.split(' ');
+    const amount = Number(amountAsString);
+    if (isNumber(amount)) {
+        //just assume name is okay (actually it can be empty)
+        fs.writeFile(FILENAME, input + '\n', { flag: 'a+' }, callMeWhenDoneWritingFile); //blocking and synchronous operation
+    }
+    else {
+        console.log('Amount is not a number');
+    }
+    displayDebtors();
+}
---
+++ src/lab01_2.ts
@@ -0,0 +1,79 @@
+// USING PROMISES
+
+import * as fs from 'node:fs/promises';
+
+import * as readLineSync from 'readline-sync';
+
+const FILENAME = 'debts02.txt';
+
+function isNumber(amount: number | string) {
+  return !Number.isNaN(amount);
+}
+
+function writeFile() {
+  return new Promise<void | string>((resolve, reject) => {
+    function promptInput() {
+      const input = readLineSync.question();
+
+      if (input === 'end') {
+        resolve();
+        return;
+      }
+
+      if (!input.includes(' ')) {
+        console.log('Invalid input format.');
+        promptInput();
+        return;
+      }
+
+      const inputArr = input.split(' ');
+      const name: string = inputArr[0];
+      const amount: number = Number(inputArr[1]);
+
+      if (!isNumber(Number(name)) && isNumber(amount)) {
+        fs.writeFile(FILENAME, input + '\n', { flag: 'a+' })
+          .then(() => {
+            promptInput();
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      } else {
+        console.log('Invalid input. Please try again.');
+        promptInput();
+      }
+    }
+
+    promptInput();
+  });
+}
+
+function readFile() {
+  return new Promise<string>((resolve, reject) => {
+    fs.readFile(FILENAME, 'utf-8')
+      .then((filecontent) => {
+        resolve(filecontent);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+console.log('Please input debtor NAME and debtor AMOUNT:');
+console.log('Type "end" to stop the program.');
+console.log('-'.repeat(100));
+
+writeFile()
+  .then(() => {
+    console.log('File written successfully.');
+    return readFile();
+  })
+  .then((filecontent) => {
+    console.log('-'.repeat(100));
+    console.log('List of Debtors and their Debts:');
+    console.log(filecontent);
+  })
+  .catch((error) => {
+    console.log('There was an error: \n', error);
+  });
